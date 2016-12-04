8ad4029958a0eb1703f2ffa9361862d31d7ed8d1
Tamas Vajk <tamas.vajk@gmail.com>
2016-12-04 21:00:37 +0100

    Gallery popup

---
+++ js/popup.js
@@ -0,0 +1,8 @@
+$('.popup-gallery').magnificPopup({
+  delegate: 'a', // child items selector, by clicking on it popup will open
+  type: 'image',  
+    gallery: {
+      enabled: true
+    },
+  // other options
+});
