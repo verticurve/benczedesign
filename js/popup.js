ca3e6668cd2e65962cb7b161188d48c76a61c263
Tamas Vajk <tamas.vajk@gmail.com>
2017-05-28 14:41:11 +0200

    Cleanup template code

--- js/popup.js
+++
@@ -1,8 +0,0 @@
-$('.popup-gallery').magnificPopup({
-    delegate: 'a', // child items selector, by clicking on it popup will open
-    type: 'image',
-    gallery: {
-        enabled: true
-    },
-    // other options
-});
