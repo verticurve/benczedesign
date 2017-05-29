fb8534fbc57ae5e603d9bf4fd7694715281e2eb6
Tamas Vajk <tamas.vajk@gmail.com>
2017-05-29 23:29:35 +0200

    Display images in alphabetical order

---
+++ _includes/gallery-sub.js
@@ -0,0 +1,15 @@
+{% for category in site.data.gallery_categories %}
+var {{category.folder}} = [{% 
+    for image in site.static_files %}{% 
+        assign basePath = '/img/gallery/' | append: {{page.gallery-folder}} | append: '/' | append: {{category.folder}} | append: '/large' %}{% 
+        if image.path contains basePath %}{% 
+            assign path = image.path %}{% 
+            assign folders = path | split: "/" | reverse %}{% 
+            assign sizeX = folders[1] | split: "x" | first %}{% 
+            assign sizeY = folders[1] | split: "x" | last %}
+{ src: '{{ path }}', w: {{sizeX}}, h: {{sizeY}} },{% 
+    endif %}{% 
+endfor %}
+];{% 
+endfor %}
+{% include gallery.js %}
--- _includes/gallery.js
+++ _includes/gallery.js
@@ -3,6 +3,18 @@ function openGallery(items) {
     var options = {
         index: 0 
     };
-    var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
+    // todo: sort should not be done on client side:
+    var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items.sort(compare), options);
     gallery.init();
+}
+function compare(a, b) {
+    var aSplit = a.src.split('/');
+    var aFileName = aSplit[aSplit.length - 1];
+    var bSplit = b.src.split('/');
+    var bFileName = bSplit[bSplit.length - 1];
+    if (aFileName < bFileName)
+        return -1;
+    if (aFileName > bFileName)
+        return 1;
+    return 0;
 }
