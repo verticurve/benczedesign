f5edf385a2177d2d1344dcc061bc1cd72d58a1d0
Tamas Vajk <tamas.vajk@gmail.com>
2017-06-14 18:53:40 +0200

    Clean liquid script to list images

--- _includes/gallery-sub.js
+++ _includes/gallery-sub.js
@@ -1,14 +1,13 @@
-{% for category in data %}
+{% for category in data %}{% 
+     assign basePath = '/img/gallery/' | append: {{page.gallery-folder}} | append: '/' | append: {{category.folder}} | append: '/large' %}{%
+     assign images = site.static_files | where_exp: "item", "item.path contains basePath" %}
 var {{category.folder}} = [{% 
-    for image in site.static_files %}{% 
-        assign basePath = '/img/gallery/' | append: {{page.gallery-folder}} | append: '/' | append: {{category.folder}} | append: '/large' %}{% 
-        if image.path contains basePath %}{% 
-            assign path = image.path %}{% 
-            assign folders = path | split: "/" | reverse %}{% 
-            assign sizeX = folders[1] | split: "x" | first %}{% 
-            assign sizeY = folders[1] | split: "x" | last %}
-{ src: '{{ path }}', w: {{sizeX}}, h: {{sizeY}} },{% 
-    endif %}{% 
+    for image in images %}{% 
+        assign path = image.path %}{% 
+        assign folders = path | split: "/" | reverse %}{% 
+        assign sizeX = folders[1] | split: "x" | first %}{% 
+        assign sizeY = folders[1] | split: "x" | last %}
+{ src: '{{ path }}', w: {{sizeX}}, h: {{sizeY}} },{%
 endfor %}
 ];{% 
 endfor %}
